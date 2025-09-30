d66559e4b41e6e4326794bff1358fb5a0c6046f9
gaidukev <90803020+gaidukev@users.noreply.github.com>
2025-09-29 20:47:52 -0600

    added unit tests

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn count_bytes(file_content: &String) -> usize {
         .len()  
 }
 
-/// Counts the lines in a string
+/// Counts the number of newline characters in a string
 /// 
 /// # Arguments
 /// * `file_content` - Reference to the string to count lines in
@@ -23,7 +23,7 @@ fn count_bytes(file_content: &String) -> usize {
 fn count_lines(file_content: &String) -> usize {
     file_content
         .lines()
-        .count()
+        .count() 
 }
 
 /// Counts the words in a string.
@@ -161,4 +161,150 @@ fn main() {
             panic!("Invalid number of arguments provided");
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn sample_text() -> String {
+        String::from("This ebook is for the use of anyone anywhere in the United States and
+most other parts of the world at no cost and with almost no restrictions
+whatsoever. You may copy it, give it away or re-use it under the terms
+of the Project Gutenberg License included with this ebook or online
+at www.gutenberg.org. If you are not located in the United States,
+you will have to check the laws of the country where you are located
+before using this eBook.")
+    }
+
+    fn sample_text_cyrillic() -> String {
+        String::from("Перша роль Карпентер на телебаченні була 2011 року на каналі NBC у серіалі «Закон і порядок: Спеціальний корпус», вона грала  é жертву зґвалтування Полу. Приблизно того ж року вона виступала в прямому ефірі на китайському телебаченні")
+    }
+
+    fn sample_text_empty() -> String {
+        String::from("")
+    }
+
+    fn sample_text_whitespace() -> String {
+        String::from(" her parts of the world at no cost and with almost no restrictions
+whatsoever. You    
+ 
+her parts of the world at no cost and with almost no restrictions
+whatsoever  
+ 
+	 
+
+")
+    }
+
+    fn sample_text_korean() -> String {
+        String::from("2013년 1월, 카펜터는 《보이 미트 월드》의 스핀오프인 디즈니 채널 시리즈 
+《라일리의 세상》에서 마야 하트 역으로
+ 캐스팅되었다")
+    }
+
+    #[test]
+    fn test_count_bytes_simple() {
+        let test_string = sample_text();
+        assert_eq!(count_bytes(&test_string), 442); 
+    }
+    #[test]
+    fn test_count_bytes_cyrillic() {
+        let test_string = sample_text_cyrillic();
+        assert_eq!(count_bytes(&test_string), 421); 
+    }
+    #[test]
+    fn test_count_bytes_empty() {
+        let test_string = sample_text_empty();
+        assert_eq!(count_bytes(&test_string), 0); 
+    }    
+    #[test]
+    fn test_count_bytes_whitespace() {
+        let test_string = sample_text_whitespace();
+        assert_eq!(count_bytes(&test_string), 174); 
+    }  
+    #[test]
+    fn test_count_bytes_korean() {
+        let test_string = sample_text_korean();
+        assert_eq!(count_bytes(&test_string), 176); 
+    }  
+
+    #[test]
+    fn test_count_lines_simple() {
+        let test_string = sample_text();
+        assert_eq!(count_lines(&test_string), 7); 
+    }
+    #[test]
+    fn test_count_lines_cyrillic() {
+        let test_string = sample_text_cyrillic();
+        assert_eq!(count_lines(&test_string), 1); 
+    }
+    #[test]
+    fn test_count_lines_empty() {
+        let test_string = sample_text_empty();
+        assert_eq!(count_lines(&test_string), 0); 
+    }
+    #[test]
+    fn test_count_lines_korean() {
+        let test_string = sample_text_korean();
+        assert_eq!(count_lines(&test_string), 3); 
+    }
+    #[test]
+    fn test_count_lines_whitespace() {
+        let test_string = sample_text_whitespace();
+        assert_eq!(count_lines(&test_string), 8); 
+    }
+
+    #[test]
+    fn test_count_words_simple() {
+        let test_string = sample_text();
+        assert_eq!(count_words(&test_string), 82); 
+    }
+    #[test]
+    fn test_count_words_cyrillic() {
+        let test_string = sample_text_cyrillic();
+        assert_eq!(count_words(&test_string), 36); 
+    }
+    #[test]
+    fn test_count_words_empty() {
+        let test_string = sample_text_empty();
+        assert_eq!(count_words(&test_string), 0); 
+    }
+    #[test]
+    fn test_count_words_korean() {
+        let test_string = sample_text_korean();
+        assert_eq!(count_words(&test_string), 16); 
+    }
+    #[test]
+    fn test_count_words_whitespace() {
+        let test_string = sample_text_whitespace();
+        assert_eq!(count_words(&test_string), 29); 
+    }
+
+    #[test]
+    fn test_count_chars_simple() {
+        let test_string = sample_text();
+        assert_eq!(count_chars(&test_string), 442); 
+    }
+    #[test]
+    fn test_count_chars_cyrillic() {
+        let test_string = sample_text_cyrillic();
+        assert_eq!(count_chars(&test_string), 234); 
+    }
+    #[test]
+    fn test_count_chars_empty() {
+        let test_string = sample_text_empty();
+        assert_eq!(count_chars(&test_string), 0); 
+    }
+    #[test]
+    fn test_count_chars_korean() {
+        let test_string = sample_text_korean();
+        assert_eq!(count_chars(&test_string), 74); 
+    }
+    #[test]
+    fn test_count_chars_whitespace() {
+        let test_string = sample_text_whitespace();
+        assert_eq!(count_chars(&test_string), 174); 
+    }
+
 }
